c45e7d97f6dc4cda4f5989defed8158df604f201
Moritz <moritz.gebauer@stud.fra-uas.de>
2022-07-23 21:41:00 +0200

    update: rewriting the loopString

--- mipsToupper_tolower.asm
+++ mipsToupper_tolower.asm
@@ -39,32 +39,34 @@
   
  # setting counter variables
  li $t0, 0 		# setting t0 to 0
+
  
- loopString:
-  lbu $t1, inputString ($t0)	# fetching input string[$t0] -> char in $t1
-  beq $t1, 10, exit
-  bgt $t1, 64, nextA		# branch if > A
-   toLarge:			
-    bgt $t1, 96, nexta		# branch if > a
-    addi $t0, $t0, 1		# adding 1 to $t0
-    j loopString		
-  nextA:
-   blt $t1, 91, tolower		# branch if < Z
-   j toLarge			
-  nexta:
-   blt $t1, 123, toupper	# branch if < z
-   addi $t0, $t0, 1		# adding 1 to $t0
-   j loopString
- 
- toupper:
-  sub  $t2, $t1, 32
-  sb $t2, outputString ($t0) 	#storing byte in OutputString
-  addi $t0, $t0, 1		#increment counter after uppering
-  
- tolower:
-  addi $t2, $t1, 32
-  sb $t2, outputString ($t0)	#storing byte in OutputString
-  addi $t0, $t0, 1
+   
+# loopString:
+#  lbu $t1, inputString ($t0)	# fetching input string[$t0] -> char in $t1
+#  beq $t1, 10, exit
+#  bgt $t1, 64, nextA		# branch if > A
+#   toLarge:			
+#    bgt $t1, 96, nexta		# branch if > a
+#    addi $t0, $t0, 1		# adding 1 to $t0
+#    j loopString		
+#  nextA:
+#   blt $t1, 91, tolower		# branch if < Z
+#   j toLarge			
+#  nexta:
+#   blt $t1, 123, toupper	# branch if < z
+#   addi $t0, $t0, 1		# adding 1 to $t0
+#   j loopString
+# 
+# toupper:
+#  sub  $t2, $t1, 32
+#  sb $t2, outputString ($t0) 	#storing byte in OutputString
+#  addi $t0, $t0, 1		#increment counter after uppering
+#  
+# tolower:
+#  addi $t2, $t1, 32
+#  sb $t2, outputString ($t0)	#storing byte in OutputString
+#  addi $t0, $t0, 1
   
  exit:
   la $a0, outputString
