4e3aabeb54a9f6baeb525afc2a1ee855ffd8f58f
Moritz <moritz.gebauer@stud.fra-uas.de>
2022-07-24 15:22:35 +0200

    update: trying to figure out /n in ascii

--- mipsTestProg.asm
+++ mipsTestProg.asm
@@ -1,6 +1,22 @@
 .data
  m: .asciiz "         "
+ m2: .asciiz "gib was ein"
 .text
- la $t0, m 
- li $a0, 12212
- sw  $a0, 0($t0) 
+
+ my_main:
+  	la $a0, m2
+	li $v0, 4
+  	syscall
+  
+  	la $a0, m
+  	li $a1, 10
+  	li $v0, 8
+  	syscall
+  	
+  	li $a1, 0
+  	 loop:
+  	  lbu  $t0, m($a1)
+  	  addi $a1, $a1, 1
+  	  j loop 
+ 	 
+ 	
