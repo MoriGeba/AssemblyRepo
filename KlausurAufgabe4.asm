23a80ea2670dc27e5b5473f15aaa964fbc632f9c
Moritz <moritz.gebauer@stud.fra-uas.de>
2022-07-25 13:54:24 +0200

    created: KlausurAufgabe4 (Wortvergleich)

---
+++ KlausurAufgabe4.asm
@@ -0,0 +1,10 @@
+.data
+ inputString1: .asciiz "                                        "
+ inputString2: .asciiz "                                        "
+ outPromp1: .asciiz " Geben sie ein Wort ein (max. 50 chars): "
+ outPromp2: .asciiz " Geben sie ein zweites Wort ein (max. 50 chars) : "
+ solvPromp: .asciiz " Die Worte sind bis zu "
+ solvPromp2: .asciiz " gleich "
+ 
+.text
+ 
